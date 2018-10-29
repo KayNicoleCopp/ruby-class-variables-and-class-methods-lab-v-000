581b9c723f4b2388da49cb18b93dc465643eb1fc
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-29 14:39:04 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+attr_accessor :name, :artist, :genre
+@@count = 0
+@@genres = []
+@@artists = []
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count += 1
+  @@artist << artist
+  @@genres << genre
+end
+
+def self.count
+  @@count
+end
+
+def self.genres
+  @@genres.uniq!
+end
+
+def self.artists
+  @@artists.uniq!
+end
+
+def self.genre_count
+  genre_count = {}
+  @@genres.each do |genre|
+    if genre_count[genre]
+      genre_count[genre] +=1
+    else
+      genre_count[genre] = 1
+    end
+  genre_count
+end
